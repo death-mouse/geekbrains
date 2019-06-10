50e09907d12ae17937bc6c8b816f6b183a0408ba
Dmitry Astakhov <deamouse@outlook.com>
2019-06-10 20:34:29 +0300

    Update job6.php

--- 2 lesson/job6.php
+++ 2 lesson/job6.php
@@ -1,11 +1,24 @@
 <?php
 	function power($val, $pow)
 	{
-		if((int)$pow !== 1){
-			return $val * power($val, $pow - 1);
-		} else {
+		
+		if ($val == 0) {
+			return 0;
+		}
+		elseif ($val == 1){
+			return 1;
+		}
+		
+		if ($pow == 0) {
+			return 1;
+		}
+		elseif ($pow == 1) {
 			return $val;
 		}
+		elseif ($pow < 0) {
+			return power(1/$val, -$pow);
+		}
+		return $val*power($val, $pow-1);
 	}
 	$a = 5;
 	$b = 10;
